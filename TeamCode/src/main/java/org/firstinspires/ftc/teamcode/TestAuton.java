404b0cf443418bd553c285dbff8c61000fd58317
Samuel Honor <shonor47@gmail.com>
2018-10-08 14:39:01 -0400

    Few more CV test Changes, will test this week

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
@@ -1,18 +1,59 @@
 package org.firstinspires.ftc.teamcode;
 
 //import com.qualcomm.robotcore.eventloop.opmode;
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 /**
  * Created by Sam on 10/7/2018.
  */
 @Autonomous
-public class TestAuton extends LinearOpMode {
+public class TestAuton extends OpMode {
 
+    private GoldAlignDetector detector;
 
+    @Override
+    public void init(){
+        telemetry.addData("Status", "DogeCV 2018.0 - Gold Align Example");
 
-    public void runOpMode(){
+        detector = new GoldAlignDetector();
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        detector.useDefaults();
+
+        // Optional Tuning
+        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.4; // How much to downscale the input frames
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        detector.maxAreaScorer.weight = 0.005;
+
+        detector.ratioScorer.weight = 5;
+        detector.ratioScorer.perfectRatio = 1.0;
+
+        detector.enable();
+    }
+
+    @Override
+    public void start() {
 
     }
+
+    @Override
+    public void loop(){
+        telemetry.addData("IsAligned" , detector.getAligned()); // Is the bot aligned with the gold mineral
+        telemetry.addData("X Pos" , detector.getXPosition()); // Gold X pos.
+
+    }
+
+    @Override
+    public void stop() {
+        detector.disable();
+    }
+
 }
