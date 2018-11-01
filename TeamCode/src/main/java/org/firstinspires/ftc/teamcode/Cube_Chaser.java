81632b112a821b0aae1f043eb3647c41436df5d8
BFPC-MXL52621NP\dfrcs_it <shonor47@gmail.com>
2018-11-01 12:35:27 -0700

    Completed Cube_Chaser autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cube_Chaser.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cube_Chaser.java
@@ -1,21 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
 import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous
-public class Cube_Chaser extends LinearOpMode {
+public class Cube_Chaser extends OpMode {
     private DcMotor mtr_left;
     private DcMotor mtr_right;
     private GoldAlignDetector gold;
 
     @Override
-    public void runOpMode() {
+    public void init(){
         mtr_left = hardwareMap.get(DcMotor.class, "mtr_left");
         mtr_right = hardwareMap.get(DcMotor.class, "mtr_right");
 
@@ -24,6 +26,41 @@ public class Cube_Chaser extends LinearOpMode {
 
         gold = new GoldAlignDetector();
         gold.init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        gold.useDefaults();
+
+        gold.alignSize = 200; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        gold.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        //gold.downscale = 0.4; // How much to downscale the input frames
+
+        gold.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //gold.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        gold.maxAreaScorer.weight = 0.005;
+
+        gold.ratioScorer.weight = 5;
+        gold.ratioScorer.perfectRatio = 1.0;
+
+        gold.enable();
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+        if(gold.getXPosition() > gold.alignSize){
+            mtr_right.setPower(0.75);
+            mtr_left.setPower(0.65);
+        }
+        else if(gold.getXPosition() < gold.alignSize){
+            mtr_right.setPower(0.65);
+            mtr_left.setPower(0.75);
+        }
+        else{
+            mtr_right.setPower(0.7);
+            mtr_left.setPower(0.7);
+        }
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuton.java
@@ -27,7 +27,7 @@ public class TestAuton extends OpMode {
         // Optional Tuning
         detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
         detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
-        detector.downscale = 0.4; // How much to downscale the input frames
+        //detector.downscale = 0.4; // How much to downscale the input frames
 
         detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
         //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
