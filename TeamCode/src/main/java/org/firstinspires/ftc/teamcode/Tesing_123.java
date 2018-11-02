14b5a63120a5553531715371cf0a782519a09f43
feehan_botics <shonor47@gmail.com>
2018-11-02 12:29:51 -0700

    showed how to make opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tesing_123.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tesing_123.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+    @Disabled
+    @Autonomous
     public class Tesing_123 extends LinearOpMode {
         @Override
+        
         public void runOpMode() {
 
         }
