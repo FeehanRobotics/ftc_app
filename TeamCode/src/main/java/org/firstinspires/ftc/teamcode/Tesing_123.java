d26c339d46fd54d234da1e8ebcdab13776d23622
feehan_botics <shonor47@gmail.com>
2018-11-13 04:33:08 -0800

    showing jimmy how to cod

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tesing_123.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tesing_123.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
     @Autonomous
     public class Tesing_123 extends LinearOpMode {
         @Override
-        
+
         public void runOpMode() {
 
         }
